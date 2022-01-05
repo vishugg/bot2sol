cff4989f0c1410e20796b274bb6332eb8bd704b3
vishugg <88967649+vishugg@users.noreply.github.com>
2022-01-05 17:10:21 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://nft:vishu@paid1.qldpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+
+bot_token:'1997623130:AAHeHwDSc93YST1ASGH6j9ZJJwmh3Z9UzOc',//bot token
+
+bot_name:'LTC_HYPER_bot',//bot username without @
+
+bot_admin:1891485685,// admin telegram id
+
+channelsList : ['@AirdropThumb' , '@Crypto_FTW' , '@ORIGINAL_AIRDROPZ' , '@LegitAirdrops010' , '@BSC_Legend' , '@SPECIAL_AIRDROPS' , '@Legit_BotsOnly' , '@FTWPayouts'],// channels list
+
+channelscheck : ['@AirdropThumb' , '@Crypto_FTW' , '@FTWPayouts'],
+
+reffer_bonus:0.000001,// refferal bonus amount
+
+min_wd:0.000001,//min withdrawal amount
+
+daily_bonus:0.000001,// daily bonus amount
+
+currency:'LTC', //bot cuurency
+
+cb_api_key:'nnDmgJkBtdl5mRHS',//coinbase api key
+
+cb_api_secret:'YicseKdfwzob1HYy6H1dgERAcvU7EZ5m',// coinbase secret key
+
+cb_account_id:'c332ae4a-9d69-583b-93ae-3926fb5aae07',// counbase account id for the bot currency
+
+payment_channel:'@FTWPayouts'// payment channel username
+
+}
